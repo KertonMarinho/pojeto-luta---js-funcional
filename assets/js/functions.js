bcde3092d8962d2249702e38b236cca6557f4ad1
Kerton marinho <kerton.marinho@outlook.com>
2023-03-09 21:23:56 -0300

    criando elementos

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,13 @@
 //personagen padrão
 const defaultCharacter  = {
-    name: "",
+    name: '',
     life: 1,
     maxLife: 1,
     attack : 0,
     defense :0
 }
 //pesonagem guerreio(knight)
-function createKnight = (name) => {
+const createKnight = (name) => {
     return {
         ...defaultCharacter,
         name,
@@ -41,7 +41,7 @@ const createLitleMonster = () => {
     }
 }
 //personagem Monstro
-const createBigMonster =  = () {
+const createBigMonster= () => {
     return {
         ...defaultCharacter,
         name: 'Big Monster',
@@ -49,5 +49,41 @@ const createBigMonster =  = () {
         maxLife: 120,
         attack : 16,
         defense:  6
+    }
+}
+const stage = {
+    fighter1: null,
+    fighter2: null,
+    fighter1E1: null,
+    fighter2E1: null,
+
+    start(fighter1, fighter2,fighter1E1, fighter2E1){
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1E1 = fighter1E1;
+        this.fighter2E1 = fighter2E1;
+        this.fighter1E1.querySelector('.attackButton').addEventListener('click',()=> this.doAttack(this.fighter1,  this.fighter2) );
+        this.fighter1E1.querySelector('.attackButton').addEventListener('click',()=> this.doAttack(this.fighter2,  this.fighter1) );
+
+        this.update();
+    },
+    update(){
+        //atualiza as informações do fighter 1
+        this.fighter1E1.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
+        //CALCULO DA VIDA
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife)* 100;
+        //para aparecer a porcentagem
+        this.fighter1E1.querySelector('.bar').style.width = `${f1Pct}%`;
+        //atuaLIZA AS INFORMAÇÕES DO FIGTHER 2
+        this.fighter2E1.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
+         //CALCULO DA VIDA
+         let f2Pct = (this.fighter2.life / this.fighter2.maxLife)* 100;
+         //para aparecer a porcentagem
+         this.fighter2E1.querySelector('.bar').style.width = `${f2Pct}%`;
+
+    
+    },
+    doAttack(attacking, attacked){
+
     }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 const char =createKnight('Bonieky');
 const  monster = createLitleMonster();
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+
+);
